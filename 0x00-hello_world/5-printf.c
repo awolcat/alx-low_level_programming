5cfbb31e3e5b057f03ed044efb5379657a8bda75
Albert <albertirura11@gmail.com>
2023-02-16 11:45:49 -0800

    0x00, C hello world task 5 printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/**
+ * main - The program starts here
+ *
+ * Description: The function prints a string with printf
+ * Return: An integer zero
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
