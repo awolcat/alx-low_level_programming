fe7987e2a0b132495b6d562060ce4ae21bbf58bb
Albert <albertirura11@gmail.com>
2023-02-16 11:14:33 -0800

    0x00, hello world. some warnings from betty but no error

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+/**
+ * This program uses puts to print a string
+ */
+#include <stdio.h>
+
+/**
+ * The program starts here
+ */
+int main(void)
+{
+	/*initialize the string*/
+	char string[] = "\"Programming is like building a multilingual puzzle";
+	/*print the string*/
+	puts(string);
+	return (0);
+}
