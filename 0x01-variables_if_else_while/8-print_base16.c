351d4a803bc2065717d4c94d380a120fc3185f03
Albert <albertirura11@gmail.com>
2023-02-17 14:28:08 -0800

    C - Variables, if, else, while Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - marks the starting point
+ *
+ * Description: print hexadecimal numbers in lower case
+ * Return: Returns zero for success value
+ */
+
+int main(void)
+{
+	int a = 0;
+	int b = 0;
+
+	while (a <= 9)
+{
+	putchar(48 + a);
+	a++;
+}
+	while (b <=5)
+{
+	putchar(97 + b);
+	b++;
+}
+	putchar('\n');
+	return (0);
+}
