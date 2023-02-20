20a9556ba30167ccf176b9e9b3f057da0c3794bd
Albert <albertirura11@gmail.com>
2023-02-20 01:29:28 -0800

    task 8: does specifying return (1) automatically produce an error?

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
-
+/**
+ * main - print string to standard error
+ *
+ * Description: return value 1 is a failure value? prints to stderr
+ * Return: return 1 failure value
+ */
 int main(void)
 {
 	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
-	return(1);
+	return (1);
 }
