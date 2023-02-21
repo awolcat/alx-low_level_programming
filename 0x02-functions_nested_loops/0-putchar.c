58ea6ca50cba52f0c3a3b39839ea09e6b5c9879b
Albert <albertirura11@gmail.com>
2023-02-21 06:27:53 -0800

    C functions and nested loops task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - calls a function to print desired output
+ *
+ * Description: same function is called 8 times
+ * Return: On success 0
+ */
+int main(void)
+
+{
+	_putchar ('_');
+	_putchar ('p');
+	_putchar ('u');
+	_putchar ('t');
+	_putchar ('c');
+	_putchar ('h');
+	_putchar ('a');
+	_putchar ('r');
+	_putchar ('\n');
+	return (0);
+
+
+}
+
