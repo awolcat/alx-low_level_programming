6e7364a278c79b007abab6be0f34f260599ffb5b
Albert <albertirura11@gmail.com>
2023-02-21 08:30:45 -0800

    task 1

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - check code
+ *
+ * Description: print alphabet 10x
+ * Return: Success 0
+ */
+
+int main(void)
+
+{
+	int c = 'a';
+
+	for (i = 0;i <=9; i++)
+{	
+	for (c = 'a'; c <= 'z'; c++)
+{
+	print_alphabet_x10(c);
+}
+}
+	return(0);
