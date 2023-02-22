1ade956a38f4fea61c28a4cf2d2ae8a1f0de4e94
Albert <albertirura11@gmail.com>
2023-02-22 08:21:13 -0800

    0x02 functions task 7 for linter

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,19 +4,19 @@
  * print_last_digit - print last digit of any number
  * @j: the input number
  *
- * Description: finds and returns the last digit of any number as a positive no.
+ * Description: finds and returns the last digit of any no as a positive no.
  * Return: returns the last digit as a positive
  */
 int print_last_digit(int j)
 
-{	
-	int n;
-	
-	n = j % 10;
-	
-	if (n < 0)
 {
-	n = -n;
+int n;
+
+n = j % 10;
+
+if (n < 0)
+{
+n = -n;
 }
-	return (n);
+return (n);
 }
