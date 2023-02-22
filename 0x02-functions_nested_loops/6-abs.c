097002ce5d48f5dae66a5b58fff23f669db941ff
Albert <albertirura11@gmail.com>
2023-02-22 08:50:22 -0800

    task 6 abs function

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include "main.h"
 #include <stdlib.h>
-
+/**
+ * _abs - prnt absolute value of given digit
+ * @j: signed or unsigned number
+ *
+ * Description: prints absolute value
+ * Return: return absolute value
+ */
 int _abs(int j)
 {
 	int n;
+
 	n = abs(j);
-	
+
 	return (n);
 }
