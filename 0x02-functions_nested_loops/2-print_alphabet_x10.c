065685995f5734957a9be9bbc261371c14514b98
Albert <albertirura11@gmail.com>
2023-02-21 18:02:15 -0800

    functions task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,11 +13,12 @@ void print_alphabet_x10(void)
 	char c = 'a';
 	int i;
 
-	for (i = 0;i <=9; i++)
-{	
+	for (i = 0; i <= 9; i++)
+{
 	for (c = 'a'; c <= 'z'; c++)
 {
-	_putchar(c);
+	_putchar (c);
 }
+	_putchar ('\n')
 }
 }
