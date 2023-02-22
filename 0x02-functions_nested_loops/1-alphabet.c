a8de26c64da8d02009672448254503dcea2e7102
Albert <albertirura11@gmail.com>
2023-02-21 18:24:09 -0800

    are we just defining functions?

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <main>
+
+/**
+ * print_alphabet - starting point
+ *
+ * Description: print alphabet
+ */
+
+void print_alphabet(void)
+
+{
+	char c = 'a';
+
+	while (c <= 'z')
+{
+		_putchar(c);
+	c++;
+}
+}
