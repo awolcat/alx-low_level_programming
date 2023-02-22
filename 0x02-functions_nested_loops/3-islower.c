25602be83237619aa3642ee61ee10954844617dd
Albert <albertirura11@gmail.com>
2023-02-21 18:40:48 -0800

    functions task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main"
+
+/**
+ * _islower - checks if a letter c is lower case
+ *
+ * Description: accepts an int and checks case
+ * Return: return 1 for fail. ie lower case found
+ */
+int _islower(int c)
+
+{
+
+	if (c >= 97 && c <= 122)
+		return (1);
+}
