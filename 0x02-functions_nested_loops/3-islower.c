4cac2350050f4ee5f0c7d649dcce0fc0319cde9b
Albert <albertirura11@gmail.com>
2023-02-22 05:20:49 -0800

    0x02 functions task 3: 1 if statement, 2 return statements

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,16 +1,22 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * _islower - checks if a letter c is lower case
+ * @c: an integer value
  *
  * Description: accepts an int and checks case
  * Return: return 1 for fail. ie lower case found
  */
 int _islower(int c)
 
+
+
 {
 
 	if (c >= 97 && c <= 122)
+{
+	return (1);
 
-	return 1;
+}
+return (0);
 }
