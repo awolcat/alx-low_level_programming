9cb483a1ceeb3105fb648cdcacfa22d0ec6fcecc
Albert <albertirura11@gmail.com>
2023-02-23 09:29:55 -0800

    0x04: task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - print n lines
+ * @n: int that controls no of lines
+ *
+ * Description: print n lines
+ * Return: nothing
+ */
+void print_diagonal(int n)
+
+{
+	int i;
+	int space;
+
+	for (i = 0; i < n; i++)
+{
+		if (n > 0)
+{
+	for (space = 0; space <= i; space++)
+{
+		_putchar(' ');
+}
+		_putchar (92);
+
+}
+
+	_putchar('\n');
+}
+	 if (n <= 0)
+        _putchar ('\n');
+}
