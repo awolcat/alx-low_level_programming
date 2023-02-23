da6226a580827ba9a350949d8e4127a2c6d22ae9
Albert <albertirura11@gmail.com>
2023-02-23 02:50:03 -0800

    0x04 more functions task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * _isupper - checks for upper or lower case
+ * @c: Function accepts an int as input
+ *
+ * Description: Check case
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+
+{
+	while (c >= 65 && c <= 90)
+	
+		return (1);
+	
+	while (c >= 97 && c <= 122)
+	
+		return (0);
+return (0);	
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory includes tasks from an ALX project on functions and nested loops
