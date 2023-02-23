6da8324128076bf8910a235fd7d1cfa8672883a8
Albert <albertirura11@gmail.com>
2023-02-23 07:56:43 -0800

    0x04 functions task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * print_line - print n lines
+ * @n: int that controls no of lines
+ *
+ * Description: print n lines
+ * Return: nothing
+ */
 void print_line(int n)
 
 {
@@ -10,10 +17,8 @@ void print_line(int n)
 {
 		if (n > 0)
 		_putchar ('_');
-		else
-		_putchar ('\n');
-
 }
+
 		_putchar ('\n');
 }
 
