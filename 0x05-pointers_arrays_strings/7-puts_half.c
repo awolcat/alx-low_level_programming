5f489cb58e9777c6ab6af9db9e0c9817795858c4
Albert <albertirura11@gmail.com>
2023-02-27 12:00:36 -0800

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - print last half of a string
+ * @str: the input string
+ * 
+ * Description: prints last half of string
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	unsigned int i, n;
+
+	for (i = 0; i <= strlen(str); i++)
+	{
+		if (strlen(str) % 2 == 0)
+		{
+			n = strlen(str) / 2;
+			_putchar(str[n + i]);
+		}
+		else if (strlen(str) % 2 == 1)
+		{
+			n = (strlen(str) - 1) / 2;
+			_putchar(str[n + i]);
+		}
+	}
+	_putchar('\n');
+}
