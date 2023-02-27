4c3e38934bb9e83d774dbf63eec598cf32f5fd6f
Albert <albertirura11@gmail.com>
2023-02-27 08:30:46 -0800

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @str: input string
+ *
+ * Description: Prints one char at a time
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; *(str + i) != '\0'; i++)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
