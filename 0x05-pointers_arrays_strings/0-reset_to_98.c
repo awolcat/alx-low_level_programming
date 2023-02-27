7c18451e97b178706d824118f93dd8f6b3c06aea
Albert <albertirura11@gmail.com>
2023-02-27 06:11:33 -0800

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+ * reset_to_98 - reset value of parameter to 98
+ * @*n: an address
+ *
+ * Description: resset value using pointer
+ * Return: void
+ */
 
 void reset_to_98(int *n)
 
