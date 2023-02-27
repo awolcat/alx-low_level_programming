b5a1534d7472d9fdf281592e7df895e7727cac33
Albert <albertirura11@gmail.com>
2023-02-27 13:14:13 -0800

    task 6r

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,10 @@
+char *_strcpy(char *dest, char *src) {
+    char *dest_start = dest;
+    while (*src != '\0') {
+        *dest = *src;
+        dest++;
+        src++;
+    }
+    *dest = '\0';
+    return dest_start;
+}
