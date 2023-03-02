31562011d2a2282204cc0caac61abf9da9e394ee
Albert <albertirura11@gmail.com>
2023-03-02 07:11:48 -0800

    task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
+/**
+ * reverse_array - reverses an array
+ * @a: Array to reverse
+ * @n: number of elements to reverse
+ * Description: check function
+ * Return: void
+ */
 
 void reverse_array(int *a, int n)
 {
 	int i = 0;
-	int j;
+	int j, k;
 	int ar[100];
 
-	for  (j = (n - 1), i = 0; j >= 0; j--, i++)  
+	for  (j = (n - 1), i = 0; j >= 0; j--, i++)
 	{
 		ar[i] = a[j];
 
-
 	}
+
+	for (k = 0; k < n; k++)
+		a[k] = ar[k];
 
 }
