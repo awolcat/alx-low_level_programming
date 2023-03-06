46e79e99cfb79885d384ccbab1fba2b1ecfc3886
Albert <albertirura11@gmail.com>
2023-03-06 10:11:11 -0800

    task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * _strpbrk - find matching character
+ * @s: the string to search
+ * @accept: the character being searched for
+ * Description: Return pointer to bytes in s where found
+ * Return: A char address
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				return (s + i);
+			}
+		}
+	}
+	return (s + i);
+}
