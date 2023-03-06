fa35a708541742863c11abbc5d183cce6ef44aa7
Albert <albertirura11@gmail.com>
2023-03-06 04:58:32 -0800

    task2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * _strchr - find and return first instance of c in s
+ * @s: Input string
+ * @c: Character to check for
+ * Description: Return s but only from first instance of c
+ * Return: the rest of s from first instance of s
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+	int sz = (int)sizeof(s);
+
+	for (i = 0; i <= sz; i++)
+	{
+		if (*(s + i) == c)
+		{
+			break;
+		}
+	}
+return (s + i);
+}
