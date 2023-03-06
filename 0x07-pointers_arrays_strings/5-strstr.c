f1ca2d1be3259b83107638b64dcf69ce30544811
Albert <albertirura11@gmail.com>
2023-03-06 12:55:54 -0800

    task 6

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,17 +8,14 @@
  */
 	char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
+	int i;
 
 	for (i = 0; *(haystack + i) != '\0'; i++)
 	{
-		for (j = 0; *(needle + j) != '\0'; j++)
-		{
 			if (*(haystack + i) == (*needle))
 			{
 				return ((haystack + i));
 			}
-		}
 	}
 	return (NULL);
 }
