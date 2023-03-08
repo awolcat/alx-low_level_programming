0adf8c6f9c7ba31f1dd4318bbed39e1237416c96
Albert <albertirura11@gmail.com>
2023-03-08 04:58:18 -0800

    task 0 no new line

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts_recursion - print string recursively
+ * @s: input string
+ * Description: checkcode
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if ((*s) != '\0')
+	{
+		_putchar (*(s));
+		s++;
+		_puts_recursion(s);
+	}
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+This folder contains tasks from the ALX week 6 project on recursion in C
