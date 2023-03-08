e56e5ccb1ac10b607bd91dee5115394e3ed9ed86
Albert <albertirura11@gmail.com>
2023-03-08 07:07:25 -0800

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * _strlen_recursion - find string length
+ * @s: input string
+ * Description: check code
+ * Return: int
+ */
+int _strlen_recursion(char *s)
+{
+	if (*(s) == '\0')
+	{
+		return (0);
+	}
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
