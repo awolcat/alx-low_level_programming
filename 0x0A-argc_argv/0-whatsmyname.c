b2aebba3e3b0d8e2eab18b94b006875a1ca4fcf7
Albert <albertirura11@gmail.com>
2023-03-10 12:35:20 -0800

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print program name
+ * @argc: Argument count
+ * @argv: Argument strings array
+ * Description: check fn
+ * Return: int for success or fail
+ */
+int main(__attribute__((unused))int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
