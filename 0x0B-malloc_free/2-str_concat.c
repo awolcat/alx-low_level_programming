1e270f7db687f53074a289ef86500da089a2ab7f
Albert <albertirura11@gmail.com>
2023-03-14 06:43:44 -0700

    task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * str_concat - concatenate 2 strings in allocated memory
+ * @s1: first string
+ * @s2: second string
+ * Description: check function description
+ * Return: Pointer to allocated memory block
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i, j, k, m;
+	char *ptr;
+
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	i = i + 1;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+
+	ptr = malloc(sizeof(char) * (i + j));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (k = 0; s1[k] != '\0'; k++)
+		{
+			ptr[k] = s1[k];
+		}
+
+		for (m = 0; s2[m] != '\0'; m++)
+			ptr[k++] = s2[m];
+
+	ptr[k + 1] = '\0';
+	return (ptr);
+	}
+}
