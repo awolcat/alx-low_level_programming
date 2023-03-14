4466a4b5128ae34d861e15faead4eaa54c5fb3e9
Albert <albertirura11@gmail.com>
2023-03-14 06:47:37 -0700

    task 2: concatenate 2 str and handle str passed as NULL

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,7 +11,15 @@ char *str_concat(char *s1, char *s2)
 {
 	int i, j, k, m;
 	char *ptr;
-
+	
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 	for (i = 0; s1[i] != '\0'; i++)
 		;
 	i = i + 1;
