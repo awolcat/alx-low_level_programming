6a6d19ec0e0ab28882af27608f5714019a321d29
Albert <albertirura11@gmail.com>
2023-03-20 13:27:21 -0700

    task 2

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * init_dog - initialize var d of type struct dog
+ * @d: the variable to be initialized
+ * @name: arg that is also member of struct dog
+ * @age: arg that is also member of struct dog
+ * @owner: arg that is also member of struct dog
+ *
+ * Description: the args are the structure and variables named after members
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
