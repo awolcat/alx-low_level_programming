f09ae8f63ba40f276c235b8cab45ee93d1e53e62
Albert <albertirura11@gmail.com>
2023-03-21 16:55:37 -0700

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,7 +13,7 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newdog;
-	int i, j;
+	int i, j, n;
 	int strlen1 = 0;
 	int strlen2 = 0;
 	char *a;
@@ -24,7 +24,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	newdog = malloc(sizeof(dog_t));
 	if (newdog == NULL)
 		return (NULL);
-	a = malloc(sizeof(char) * (strlen1 + 1));
+	a = malloc(sizeof(char) * (2 * strlen1) + 2);
 	if (a == NULL)
 	{
 		free(a);
@@ -34,18 +34,24 @@ dog_t *new_dog(char *name, float age, char *owner)
 		a[i] = name [i];
 	a[i] = '\0';
 	newdog->name = a;
+	for (j = (i + 1), n = 0; j < (2 * (strlen1 + 2)); j++, n++)
+		newdog->name[j] = name[n];
+	newdog->name[j] = '\0';
 	newdog->age = age;
 
 	for (j = 0; owner[j] != '\0'; j++)
 		strlen2++;
 
-	b = malloc(sizeof(char) * (strlen2 + 1));
+	b = malloc(sizeof(char) * (2 * strlen2) + 2);
 	if (b == NULL)
 		return (NULL);
 	for (j = 0; owner[j] != '\0'; j++)
 		b[j] = owner[j];
 	b[j] = '\0';
 	newdog->owner = b;
+	for (n = (j + 1), i = 0; n < (2 * (strlen2 + 2)); n++, i++)
+		newdog->owner[n] = owner[i];
+	newdog->owner[n] = '\0';
 
 	return (newdog);
 }
