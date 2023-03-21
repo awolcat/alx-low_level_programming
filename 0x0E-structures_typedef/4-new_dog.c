c86e8ca18501d725e765fcf6dd9949cd1abd3509
Albert <albertirura11@gmail.com>
2023-03-21 15:45:33 -0700

    task 4 correction

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,8 +13,16 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newdog;
+	int strlen1 = 0;
+	int strlen2 = 0;
+	int i;
 
-	newdog = malloc(sizeof(struct dog));
+	for (i = 0; name[i] != '\0'; i++)
+		strlen1++;
+	for (i = 0; owner[i] != '\0'; i++)
+		strlen2++;
+
+	newdog = malloc(sizeof(float) + strlen1 + strlen2 + 4);
 
 	if (newdog == NULL)
 		return (NULL);
