7f8ba0b724cc4f70aaf4a8ec8e59c1e5ab64bbc8
Albert <albertirura11@gmail.com>
2023-03-22 09:35:30 -0700

    task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * int_index - find index of element that meets criteria in cmp
+ * @array: the array to inspect
+ * @size: size of array
+ * @cmp: function that does a comparison
+ *
+ * Description: Find index of element that meets cmp criteria
+ * Return: an int which is array index
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, j;
+	int *arr;
+
+	if (size <= 0)
+		return (-1);
+	if (array != NULL && cmp != NULL)
+	{
+		arr = malloc(sizeof(int) * size);
+		if (arr == NULL)
+		{
+			free(arr);
+			exit(1);
+		}
+		for (i = 0; i < size; i++)
+		{
+			arr[i] = array[i];
+		}
+		j = 0;
+		while ((*cmp)(arr[j]) < 1 && j < size)
+		{
+			(*cmp)(arr[j]);
+			j += 1;
+		}
+		if (j == (size - 1))
+			return (-1);
+	}
+	return (j);
+}
