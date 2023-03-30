b37c665e063f7576adf5ece6818fa7505df24ade
Albert <albertirura11@gmail.com>
2023-03-30 15:17:02 -0700

    free singly linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_list - free singly linked list
+ * @head: start node of list
+ *
+ * Description: free list
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t buffer;
+
+	while (head != NULL)
+	{
+		buffer = head->next;
+		free(head->str);
+		free(head);
+		head = buffer;
+	}
+}
