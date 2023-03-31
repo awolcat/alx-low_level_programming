9fd7a6548d297fde880f74a5781d11f284f150e8
Albert <albertirura11@gmail.com>
2023-03-31 08:35:59 -0700

    task 3: add node to end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -43,8 +43,8 @@ list_t *add_node_end(list_t **head, const char *str)
 		while ((temp->next) != NULL)
 			temp = temp->next;
 		temp->next = end;
-		return (end);
+		if ((temp->next) == end)
+			return (end);
 	}
-	else
-		return (NULL);
+	return (NULL);
 }
