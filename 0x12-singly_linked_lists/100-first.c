55b75b8fd27027f094b08279d80d9e8c2c35c94e
Albert <albertirura11@gmail.com>
2023-03-31 10:12:01 -0700

    100-function attribute constructor

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * first - constructor function
+ *
+ * Return: void
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
