0294eed0e6ddf33e9378e1e754ba02e0a9df08b8
Albert <albertirura11@gmail.com>
2023-04-03 15:20:49 -0700

    task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,15 +13,14 @@ void free_listint2(listint_t **head)
 
 	if (*head == NULL)
 		return;
-	else
+
+	temp = *head;
+	while (temp->next != NULL)
 	{
-		temp = *head;
-		while (temp != NULL)
-		{
-			temp_next = temp->next;
-			free(temp);
-			temp = temp_next;
-		}
-		*head = NULL;
+		temp_next = temp->next;
+		free(temp);
+		temp = temp_next;
 	}
+	free(temp);
+	*head = NULL;
 }
