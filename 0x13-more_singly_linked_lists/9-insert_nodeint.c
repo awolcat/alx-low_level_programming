c482fab02835ad93f2a93b2c2e06a66afb596e48
Albert <albertirura11@gmail.com>
2023-04-04 08:40:07 -0700

    task 9 edge case head, tail

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -34,7 +34,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		temp = temp->next;
 	}
-	if (!temp || temp->next == NULL)
+	if (!temp)
 		return (NULL);
 	temp_next = temp->next;
 	temp->next = new;
