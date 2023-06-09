8ef0fba6d269e8cbe7d13a1e1eb7bd4ed9d18a79
Albert <albertirura11@gmail.com>
2023-06-09 02:10:48 -0700

    create hash table function

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,12 +11,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = NULL;
 	hash_node_t **arr = NULL;
-/*	unsigned long int index = 0;
-*/
+	unsigned long int index = 0;
+
 	if (size == 0)
 		return (NULL);
 
-	table = malloc(sizeof(table));
+	table = malloc(sizeof(hash_node_t));
 	if (!table)
 		return (NULL);
 
@@ -27,11 +27,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	/*Initialize hash table*/
-/*	while (index < size)
+	while (index < size)
 	{
 		table->array[index] = NULL;
 		index++;
 	}
-*/	return (table);
+	return (table);
 }
 
