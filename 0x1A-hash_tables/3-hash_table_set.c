70ed4d0b58eea21ccfedce4979bc690e39c8b4be
Albert <albertirura11@gmail.com>
2023-06-09 09:16:30 -0700

    the one

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *node = NULL;
-	hash_node_t *temp = NULL;
 
 	if (!key || strlen(key) == 0)
 		return (0);
