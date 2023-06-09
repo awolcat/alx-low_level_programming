2246bb19723918564c1c95246e0a20de8bf1855d
Albert <albertirura11@gmail.com>
2023-06-09 00:22:20 -0700

    hash table create function

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+/**
+ * hash_table_t - create a hash table
+ * @size: size of the table
+ *
+ * Return: pointer to the table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table = NULL;
+	table = malloc(sizeof(table) * size);
+
+	if (!table)
+		return NULL;
+	return table;
+}
+
