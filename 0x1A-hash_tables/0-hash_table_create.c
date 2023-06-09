b680537afaf7bd5f6f5961dde2b544fc7c42e6ec
Albert <albertirura11@gmail.com>
2023-06-09 02:19:20 -0700

    write create hash table function

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,6 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = NULL;
-	hash_node_t **arr = NULL;
 	unsigned long int index = 0;
 
 	if (size == 0)
@@ -21,8 +20,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	table->size = size;
-	arr = malloc(sizeof(hash_node_t*) * table->size);
-	table->array = arr;
+	table->array = malloc(sizeof(hash_node_t *) * table->size);
 	if (!table->array)
 		return (NULL);
 
