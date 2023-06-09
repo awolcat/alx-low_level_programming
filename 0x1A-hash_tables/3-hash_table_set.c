361c545aab46d620450977199eb78d47244a6728
Albert <albertirura11@gmail.com>
2023-06-09 15:26:42 -0700

    wrote function to set hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -44,10 +44,20 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		current = current->next;
 	}
-	if (update == 0)
+	/* Add a node if key does not exist */
+	current = ht->array[index];
+	if (update != 1)
 	{
-		node->next = ht->array[index];
-		ht->array[index] = node;
+		if (current == 0)
+		{
+			node->next = NULL;
+			ht->array[index] = node;
+		}
+		else
+		{
+			node->next = ht->array[index];
+			ht->array[index] = node;
+		}
 	}
 	return (1);
 }
