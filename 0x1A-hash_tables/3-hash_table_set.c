ab1ade606d46e63a0c129ea8c4f70f0ff410327e
Albert <albertirura11@gmail.com>
2023-06-09 04:42:55 -0700

    wrote hash_table_set function to add a node at some index of the table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <string.h>
+#include "hash_tables.h"
+#include <stdio.h>
+/**
+ * hash_table_set - add node to the table
+ * @ht: pointer to the hash table
+ * @key: key of the element
+ * @value: value part of key value pair
+ *
+ * Return: 0 Success!
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *node = NULL;
+	hash_node_t *temp = NULL;
+
+	if (!key || strlen(key) == 0)
+		return (1);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	/*Initialize the node*/
+	node = malloc(sizeof(hash_node_t));
+	node->key = malloc(sizeof(char) * strlen(key) + 1);
+	node->value = malloc(sizeof(char) * strlen(value) + 1);
+	strcpy(node->key, key);
+	strcpy(node->value, value);
+	node->next = NULL;
+
+	/*Add node to hash table*/
+	if (ht->array[index] == NULL)
+	{
+		ht->array[index] = node;
+
+		printf("%s\n", ht->array[index]->value);
+	}
+	/*If index is already occupied, handle collision by chaining*/
+	else
+	{
+		temp = ht->array[index];
+		while (temp)
+		{
+			if (temp->next == NULL)
+			{
+				temp->next = node;
+			}
+			temp = temp->next;
+		}
+	}
+	return (0);
+}
